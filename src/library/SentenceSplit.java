12720cbfa558e7afcd74913c2e3b49b89c56ba11
hawn5412 <devnull@localhost>
2013-10-10 17:12:39 +0900

    주석

--- src/library/SentenceSplit.java
+++ src/library/SentenceSplit.java
@@ -4,7 +4,8 @@ public class SentenceSplit {
 	{
 		ArrayList<String> list= new ArrayList<String>();
 		StringTokenizer st = new StringTokenizer(sentence," ");
-		while(st.hasMoreElements())
+		//" "단위로 문장나눔
+		while(st.hasMoreElements())//list에 " "단위로 저장
 		{
 			list.add(st.nextToken());
 		}
@@ -13,7 +14,7 @@ public class SentenceSplit {
 	public static void main(String[] args)
 	{
 		ArrayList<String> list=split("나는 밥을 먹었다.");
-		for(int i=0; i<list.size();i++)
+		for(int i=0; i<list.size();i++)//list 내용 프린트
 		{
 			System.out.println(list.get(i));
 		}
