b9333d095e86d411fa8eeb48a4192eb35b59c1ad
Nidev Plontra <nidev.plontra@gmail.com>
2013-10-22 11:50:15 +0900

    BaseFragment 인터페이스 작성완료

--- src/libs/fragments/BaseFragment.java
+++ src/libs/fragments/BaseFragment.java
@@ -1,13 +1,38 @@
 package libs.fragments;
 
-// 작업 중 //
+import java.util.ArrayList;
+import java.util.HashMap;
 
-abstract class BaseFragment
+/*
+ * 각 프래그먼트 객체가 사용할 인터페이스를 정의하는 인터페이스 파일
+ */
+
+interface BaseFragment
 {
+	
+	// 모든 것을 구현할 필요는 없음. 
+	
+	// 분석된 텍스트 원본 자체가 저장되어있는 변수 
+	ArrayList<String> sourceTexts = new ArrayList<String>();
+	
+	//  getNextFragment()를 하면 얻는 다음 Fragment
+	int nextFragmentId = 0;
+	// 계산된 감정 평균값
+	int averageEmotionValue = 0;
+	// 분석 결과 저장을 위한 HashMap
+	HashMap<BaseFragment, Integer> fragments = new HashMap<BaseFragment, Integer>();
 
-	public BaseFragment()
-	{
-		// TODO Auto-generated constructor stub
-	}
-
+	
+	public ArrayList<String> getTexts(); // 본문 가져오기
+	public ArrayList<BaseFragment> getFragments(); // 프래그먼트 가져오기
+	public int getAverageEmotionVector(); // 평균 감정값 리턴
+	
+	public HashMap<String, Integer> getEmotionVectors(); // 분석 결과가 저장된 HashMap 전체 리턴
+	public int lengthFragments(); // HashMap 길이 리턴
+	public int lengthRemainingFragments(); // HashMap 전체 길이에서 nextFragmentId 를 뺀 값
+	public boolean isNextFragmentOK(); // 다음 프래그먼트를 가져올 수 있는지 여부 출력
+	public void resetNextFragmentPosition(); // nextFragmentId를 0으로 만듦
+	public void setIgnoreFlag(); // 무시 설정 (아직 모름)
+	
+	public BaseFragment getNextFragments(); // 다음 프래그먼트 가져오기
 }
--- src/libs/fragments/WordFragment.java
+++ src/libs/fragments/WordFragment.java
@@ -1,13 +1,86 @@
 package libs.fragments;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 // 작업 중 //
 
-public class WordFragment extends BaseFragment
+public class WordFragment implements BaseFragment
 {
 
 	public WordFragment()
 	{
 		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public ArrayList<String> getTexts()
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ArrayList<BaseFragment> getFragments()
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int getAverageEmotionVector()
+	{
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public HashMap<String, Integer> getEmotionVectors()
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int lengthFragments()
+	{
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int lengthRemainingFragments()
+	{
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public boolean isNextFragmentOK()
+	{
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void resetNextFragmentPosition()
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setIgnoreFlag()
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public BaseFragment getNextFragments()
+	{
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }
