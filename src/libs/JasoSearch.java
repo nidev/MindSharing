965a46dce980c931ec9ece5ada146327263cc783
Nidev Plontra <nidev.plontra@gmail.com>
2013-10-29 12:21:50 +0900

    JasoSearch 문제점 파악 및 코드 보강 완료. 단어 검색은 추가로 구현할 것.

--- src/libs/JasoSearch.java
+++ src/libs/JasoSearch.java
@@ -6,19 +6,47 @@ import java.io.IOException;
 
 public class JasoSearch
 {
+	/*
+	 * System.getProperty() 로는 자바 시스템 관련 설정 값들을 가져 올 수 있다.
+	 * 그 중 getProperty("user.dir")은 현재 작업 폴더를 가져오는 명령어.
+	 * 3-2-term-project 폴더 자체를 가르키므로, src폴더나 bin 폴더에 접근하려면 직접 뒤의 경로를 적어주어야한다.
+	 */
+	final static String DICTIONARY_PATH = System.getProperty("user.dir") + "\\src\\libs\\긍정 관련 단어.txt"; 
+	
 	public static void main(String[] args) throws IOException
 	{
-		BufferedReader in = new BufferedReader(new FileReader("C:\\Users\\yjpark\\Desktop\\File\\메모장\\긍정 관련 단어.txt"));
+		/*
+		// 프로퍼티 확인용 함수
+		Properties prop = System.getProperties();
+		for (Object obj: prop.keySet())
+		{
+			ELog.d(obj.toString(), obj.hashCode());
+		}
+		*/
+		
+		// 버퍼 열기
+		BufferedReader in = new BufferedReader(new FileReader(DICTIONARY_PATH));
 		
 		String str;
 		while(true)
 		{
+			// 혹시 텍스트파일이 UTF-8이 아니라 그런 것 같다.
+			// 텍스트 파일도 모두 UTF-8로 작성하거나 (즉, 이클립스 내에서 텍스트파일 작업),
+			// 윈도우즈 인코딩(MS949) 로 작성된 파일을 UTF-8로 변환하는 과정이 필요함.
 			str=in.readLine();
-			if(str==null)
-				break;
 			
-			System.out.println(str);
+			if(str==null)
+			{
+				// 문자열이 없는 경우에는 종료
+				break;
+			}
+			else
+			{
+				// 있는 경우에는?
+				System.out.println(str);
+			}
 		}
+		// 버퍼 닫기
 		in.close();
 	}
 }
