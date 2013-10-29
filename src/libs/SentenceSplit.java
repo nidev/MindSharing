eb76ece01b2c2ca3826ce816347dc258eeec62d5
hawn5412 <devnull@localhost>
2013-10-30 00:30:13 +0900

    스샷을 위해 수정

--- src/libs/SentenceSplit.java
+++ src/libs/SentenceSplit.java
@@ -18,6 +18,8 @@ public class SentenceSplit {
 	}
 	public static void main(String[] args)
 	{
+		System.out.print("문장 띄여쓰기로 나누기\n");
+		System.out.print("나는 밥을 먹었다.\n");
 		ArrayList<String> list=split("나는 밥을 먹었다.");
 		for(String x : list)//list 내용 프린트 iterator를 이용 간략화
 		{
