728713c9225676c68f70d059e6e8d2c193d096c3
Nidev Plontra <nidev.plontra@gmail.com>
2013-11-05 00:03:09 +0900

    코드 정리 및 주석 이동. 출력이 안뜨는 건, 줄에서 matches()라는 메소드를 사용하는 건 맞는데, 안에 입력된 패턴이 '기쁨,1'을 찾기에 부족한 패턴인 것 같다.

--- src/libs/JasoSearch.java
+++ src/libs/JasoSearch.java
@@ -1,6 +1,5 @@
 package libs;
 
-import java.util.regex.*;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
@@ -14,14 +13,20 @@ public class JasoSearch
 	 * 3-2-term-project 폴더 자체를 가르키므로, src폴더나 bin 폴더에 접근하려면 직접 뒤의 경로를 적어주어야한다.
 	 */
 	// 그리고 파일명은 꼭 영어로!
+	// 텍스트파일이 UTF-8이 아니라 그런 것 같다.
+	// 텍스트 파일도 모두 UTF-8로 작성하거나 (즉, 이클립스 내에서 텍스트파일 작업),
+	// 윈도우즈 인코딩(MS949) 로 작성된 파일을 UTF-8로 변환하는 과정이 필요함.
+	//str=in.readLine();
+
 	final static String DICTIONARY_PATH = System.getProperty("user.dir") + "\\src\\libs\\Positve Words.txt"; 
 	
 	public static void main(String[] args) throws IOException
 	{
+		
 		String findStr = "기쁨";
 		int lineNumber = 1;               //행 번호
 		/*
-		// 프로퍼티 확인용 함수
+		// 테스트용: 프로퍼티 확인용 함수
 		Properties prop = System.getProperties();
 		for (Object obj: prop.keySet())
 		{
@@ -29,27 +34,37 @@ public class JasoSearch
 		}
 		*/
 		
-		try{
-		// 버퍼 열기
-		BufferedReader in = new BufferedReader(new FileReader(DICTIONARY_PATH));
-		
-		String str;
-		while((str = in.readLine()) != null)
+		try
 		{
-			// 혹시 텍스트파일이 UTF-8이 아니라 그런 것 같다.
-			// 텍스트 파일도 모두 UTF-8로 작성하거나 (즉, 이클립스 내에서 텍스트파일 작업),
-			// 윈도우즈 인코딩(MS949) 로 작성된 파일을 UTF-8로 변환하는 과정이 필요함.
-			//str=in.readLine();
-			if(str.matches(findStr))
-				System.out.format("%3d: %s%n",lineNumber, str);
+			// 버퍼 열기
+			BufferedReader in = new BufferedReader(new FileReader(DICTIONARY_PATH));
 			
-			lineNumber++;                  //행 번호 증가
+			String str;
+			while((str = in.readLine()) != null)
+			{
+				// IF-ELSE: 만약 매치되지 않았을 때는 어떤 일이 일어나는걸까?
+				// while 문을 빠져나간 다음에, 결과가 있었는지 없었는지 boolean 변수로 확인시켜줄 수 있다면,
+				// 상황에 알맞는 처리를 해줄 수 있을듯.
+				
+				// matches() 메소드는 패턴을 필요로 하는데, '기쁨'이라는 단어만으로는 '기쁨,1'이 있는 줄을 매칭할 수 없을지도 모른다.
+				// findStr에 기쁨,1 을 넣어보고 돌려서, 출력이 나오는지 확인하고, 어떻게 고칠지 고민해보자.
+				if(str.matches(findStr)) 
+				{
+					System.out.format("%3d: %s%n",lineNumber, str);
+				}
+				else
+				{
+					// 만약 어떤 처리가 필요하다면
+				}
+				lineNumber++;                  //행 번호 증가
+			}
+			// 버퍼 닫기
+			in.close();
 		}
-		// 버퍼 닫기
-		in.close();
-		}
-		catch (IOException e) {
+		catch (IOException e)
+		{
 	        System.err.println(e); // 에러가 있다면 메시지 출력
+	        // e.printStackTrace(); // << 보통 이 함수를 오류 메시지 출력에 많이 이용. 참고로만 알아두삼.
 	        System.exit(1);
 	    } 
 	}
