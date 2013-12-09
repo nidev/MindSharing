690bc27e3c403e91d42298c0cf61b0f799f1af71
Nidev Plontra <nidev.plontra@gmail.com>
2013-12-09 23:59:17 +0900

    EmotionNegativeValue.java 를 EmotionPositiveValue.java를 상속하게 만들어, 이후에 코드 유지 보수를 편리하게 할 수 있도록 수정함. (이후에 기능 수정이 있을 경우, EmotionPositiveValue.java 만 수정하면 OK!)

--- src/example/ReadingResourceExample.java
+++ src/example/ReadingResourceExample.java
@@ -1,7 +1,7 @@
 /**
  * MindSharing Project
  */
-package test;
+package example;
 
 
 /**
--- src/libs/EmotionPositiveValue.java
+++ src/libs/EmotionPositiveValue.java
@@ -1,29 +1,17 @@
 package libs;
 
 import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-import libs.interfaces.*;
+import libs.interfaces.EmotionValue;
 
 //인터페이스 구현에 필요한 자료는 src/libs/interfaces/EmotionValue.java 안에 설명되어있음!
 
 public class EmotionPositiveValue implements EmotionValue
 {
-	/*
-	 * System.getProperty() 로는 자바 시스템 관련 설정 값들을 가져 올 수 있다.
-
-	 * 그 중 getProperty("user.dir")은 현재 작업 폴더를 가져오는 명령어.
-	 * 3-2-term-project 폴더 자체를 가르키므로, src폴더나 bin 폴더에 접근하려면 직접 뒤의 경로를 적어주어야한다.
-	 */
-	// 그리고 파일명은 꼭 영어로!
-	// 텍스트파일이 UTF-8이 아니라 그런 것 같다.
-	// 텍스트 파일도 모두 UTF-8로 작성하거나 (즉, 이클립스 내에서 텍스트파일 작업),
-	// 윈도우즈 인코딩(MS949) 로 작성된 파일을 UTF-8로 변환하는 과정이 필요함.
-	//str=in.readLine();
-
-	final static String DICTIONARY_PATH = System.getProperty("user.dir") + "\\src\\libs\\Positve Words.txt"; 
+	final static String DICTIONARY_PATH = "/libs/Positive Words.txt"; 
 
 	// 주의: main() 함수 여기에다가 작성하지 말자!
 	// main()으로 호출해볼 수 있는 곳: src의 'test' 폴더 안에 있는 실행기들!
@@ -72,7 +60,12 @@ public class EmotionPositiveValue implements EmotionValue
 		try
 		{
 			//버퍼 열기
-			BufferedReader in = new BufferedReader(new FileReader(DICTIONARY_PATH));
+			/*
+			 * 버퍼 여는 방법이 약간 바뀌었다
+			 * 이것은 나중에 JAR 파일로 패키징해서도 패키지 내부의 사전 파일을 안전하게 읽을 수 있도록한다.
+			 * 소스 코드 설명에 대해서는 src/examples/ 에 있는 readingResourceExample.java 를 참고
+			 */
+			BufferedReader in = new BufferedReader(new InputStreamReader(EmotionPositiveValue.class.getResourceAsStream(DICTIONARY_PATH)));
 			
 			String str;
 			while((str = in.readLine()) != null)
