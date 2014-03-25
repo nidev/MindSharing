b1e703eee96ab0087843974f449b690a48935b2b
Nidev Plontra <nidev.plontra@gmail.com>
2014-03-25 12:42:42 +0900

    새 작업을 위한 기초 패키지 생성

---
+++ src/jnu/mindsharing/appserver/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * ReSTful API를 제공하기 위한 웹서버
+ */
+/**
+ * @author nidev
+ *
+ */
+package jnu.mindsharing.appserver;
---
+++ src/jnu/mindsharing/chainengine/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 세종 말뭉치 시스템을 이용하여 감정 어휘 분석을 돕는 체인 엔진
+ */
+/**
+ * @author nidev
+ *
+ */
+package jnu.mindsharing.chainengine;
