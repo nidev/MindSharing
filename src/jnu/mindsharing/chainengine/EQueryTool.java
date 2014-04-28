cb391eea9c11199688b4f28fd3ea3644e6232d36
Nidev Plontra <nidev.plontra@gmail.com>
2014-04-28 18:42:09 +0900

    단어 분류에 필요한 메소드 몇개 추가 (현재는 STUB 코드로만 있음)

--- src/jnu/mindsharing/chainengine/EQueryTool.java
+++ src/jnu/mindsharing/chainengine/EQueryTool.java
@@ -38,6 +38,24 @@ public class EQueryTool
 		return 0;
 	}
 	
+	public static boolean isEmoticon(String word)
+	{
+		return true; // STUB: 일단 들어가는지 체크함
+	}
 	
-
+	public static boolean isEnhancer(String word)
+	{
+		return true; // STUB: 감정을 강화하는가
+	}
+	
+	public static boolean isReducer(String word)
+	{
+		return true; // STUB: 감정을 감소하는가
+	}
+	
+	public static boolean isNegativeADV(String word)
+	{
+		// 부정적인 접두 부사
+		return word.equals("안") || word.equals("아니");
+	}
 }
